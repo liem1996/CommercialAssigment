3c30a19d598f920bcece8dab4750d3f114d1b8c9
amit ben basat <amitbasat124@gmail.com>
2022-01-03 16:52:18 +0200

    adding the clients and the connect and diconnect of the clients

--- CommercialsMongoDB.js
+++ CommercialsMongoDB.js
@@ -3,7 +3,7 @@ var app = express();
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017/'
-const databaseName = 'CommercialssLiem'
+const databaseName = 'CommercialssLiem12'
 
 const path = require('path');
 
@@ -19,51 +19,42 @@ MongoClient.connect(connectionURL,{useNewUrlParser : true},(error,client) => {
        [
         {
           opentime: 2000,
-          image:"https://i.ytimg.com/vi/b7gTudFZoL8/maxresdefault.jpg",
-          Screen:1,
+          image:"https://i.ytimg.com/vi/b7gTudFZoL8/maxresdefault.jpg"
         },
         {
           opentime: 3000,
-          image:"https://static.wixstatic.com/media/9aa509_4d6b69af038e47269d1e39735987c6ed~mv2.jpg/v1/fill/w_250,h_363,al_c,q_90/9aa509_4d6b69af038e47269d1e39735987c6ed~mv2.jpg",
-          Screen:1    
+          image:"https://static.wixstatic.com/media/9aa509_4d6b69af038e47269d1e39735987c6ed~mv2.jpg/v1/fill/w_250,h_363,al_c,q_90/9aa509_4d6b69af038e47269d1e39735987c6ed~mv2.jpg"  
         },
         {
           opentime: 6000,
-          image:"https://o.quizlet.com/DkJ6OEEA6nkfdZsb7CEOiw.jpg",
-          Screen:1    
+          image:"https://o.quizlet.com/DkJ6OEEA6nkfdZsb7CEOiw.jpg" 
         },
       {
         opentime: 2000,
-        image:"https://images1.ynet.co.il/PicServer5/2018/12/13/8942861/wide-article-980.jpg",
-        Screen:2
+        image:"https://images1.ynet.co.il/PicServer5/2018/12/13/8942861/wide-article-980.jpg"
       },
       {
         opentime: 3000,
-        image:"https://cashcow-cdn.azureedge.net/images/a26817b8-bab0-4579-9fbe-9a8e27a12ce8_500.jpg",
-        Screen:2  
+        image:"https://cashcow-cdn.azureedge.net/images/a26817b8-bab0-4579-9fbe-9a8e27a12ce8_500.jpg"
 
       },
       {
         opentime: 6000,
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw9BiZB7lUGFNWWdKsMAIBPULWCA-O6yNEIw&usqp=CAU",
-        Screen:2   
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw9BiZB7lUGFNWWdKsMAIBPULWCA-O6yNEIw&usqp=CAU"
 
       },
     {
       opentime: 2000,
-      image:"https://gidigov.co.il/wp-content/uploads/2016/03/bezeq-2.jpg",
-      Screen:3
+      image:"https://gidigov.co.il/wp-content/uploads/2016/03/bezeq-2.jpg"
     },
     {
       opentime: 3000,
-      image:"https://www.makorrishon.co.il/wp-content/uploads/2018/03/1_118491090.jpg",
-      Screen:3 
+      image:"https://www.makorrishon.co.il/wp-content/uploads/2018/03/1_118491090.jpg"
 
     },
     {
       opentime: 6000,
-      image:"https://www.shotim.co.il/wp-content/uploads/nostalgic-ad-elite-instant-coffee.jpg",
-      Screen:3  
+      image:"https://www.shotim.co.il/wp-content/uploads/nostalgic-ad-elite-instant-coffee.jpg"
 
     },
 ]
@@ -72,6 +63,27 @@ MongoClient.connect(connectionURL,{useNewUrlParser : true},(error,client) => {
             return console.log('Could not insert')
         }
     })
+
+    db.collection('commercialsUsers').insertMany(
+      [
+       {
+        isConnected: false,
+        Screen: 1
+        },
+       {
+        isConnected: false,
+        Screen: 2    
+         },
+       {
+        isConnected: false,
+        Screen: 3
+       },
+]
+   , (error,result) => {
+       if (error){
+           return console.log('Could not insert')
+       }
+   })
      console.log("done entering to DB");
 
 })
--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var port=8089;
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017/'
-const databaseName = 'CommercialssLiem'
+const databaseName = 'CommercialssLiem12'
 const databasecomm = 'commercialsAdds'
+const databasecommUsers = 'commercialsUsers'
+var http = require( 'http' ).createServer(app);
+var io = require( 'socket.io' )( http );
+
 
 const path = require('path');
 
@@ -15,9 +19,7 @@ app.use("/script", express.static('./script/'));
 let db=0;
  
 var temp =[];
-var temp2 =[];
-var temp3 =[];
-
+let screen=0;
 
 MongoClient.connect(connectionURL,{useNewUrlParser : true},(error,client) => {
     if (error){
@@ -25,38 +27,97 @@ MongoClient.connect(connectionURL,{useNewUrlParser : true},(error,client) => {
     }
      db = client.db(databaseName);
 
-      db.collection(databasecomm).find({Screen:1}).toArray((error,tasks)=>{
-      temp = tasks}
-      )
+      db.collection(databasecomm).find().toArray((error,tasks)=>{
+      temp = tasks})
 
-      db.collection(databasecomm).find({Screen:2}).toArray((error,tasks2)=>{
-        temp2 = tasks2})
+      db.collection(databasecommUsers).findOne({
+        Screen:1
+          }, (error,task) => {
+        screen1=task;
+    })
 
-        db.collection(databasecomm).find({Screen:3}).toArray((error,tasks3)=>{
-          temp3 = tasks3})
+    db.collection(databasecommUsers).findOne({
+      Screen:2
+        }, (error,task2) => {
+          screen2=task2;
+  })
+
+  db.collection(databasecommUsers).findOne({
+    Screen:3
+      }, (error,task3) => {
+        screen3=task3;
 })
 
 
-app.get('/screen=1', (req, res) => {
+app.get('/screen=:screen', (req, res) => {
+  screen = req.params.screen ;
+  
     res.render('index',{
       screen:temp,
-    });
   });
+  
+  
+});
 
-  app.get('/screen=2', (req, res) => {
-    res.render('index',{
-      screen:temp2,
+
+http.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+
+});
+
+io.on( 'connection', function( socket ) {
+  console.log( "a user has connected!" );
+  if (screen == 1)
+  {
+    UserConnect(1, true);
+   }
+
+  if (screen == 2)
+  {
+    UserConnect(2, true);
+
+  }
+  if (screen == 3)
+  {
+    UserConnect(3, true);
+
+  }
+  
+  socket.on( 'disconnect', function() {
+    if (screen == 1)
+    {
+      UserConnect(1, false);
+    }
+    if (screen == 2)
+    {
+     UserConnect(2, false);
+
+    }
+    if (screen == 3)
+    {
+      UserConnect(3, false);
+
+    }
+
+    console.log( "user disconnected");
     });
-  });
+ });
 
-  app.get('/screen=3', (req, res) => {
-    res.render('index',{
-      screen:temp3,
-    });
-  });
 
-  app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`);
+});
+
+
+function UserConnect(num, b)
+{
+  
+  var myquery = { Screen: num };
+  var newvalues = { $set: { isConnected: b } };
+  db.collection(databasecommUsers).updateOne(myquery, newvalues, function(err, res) {
+  } ) ;
+}
 
   
-  });
+
+
+
+
